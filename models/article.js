e36b6c574b2ced97f2e339b038fdda1ef11ac26c
29bucuk <pinargultekin@yahoo.com>
2019-09-07 23:19:46 -0400

    everythin is working FINALLYgit add -A!

--- models/article.js
+++ models/article.js
@@ -1,10 +1,7 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
@@ -16,23 +13,19 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  },
-  // `picture` is required and of type String
-  picture: {
-      type: String,
-      required: true
-  },
-
   summary: {
     type: String,
     required: true
-  }
+  },
+  picture: {
+    type:String,
+    required: true
+  },
+  // `note` is an array that stores a Note id
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,76 +1,87 @@
-// // Grab the articles as a json
-// // $.getJSON("/articles", function (data) {
-// //     // For each one
-// //     for (var i = 0; i < data.length; i++) {
-// //         // Display the apropos information on the page
-// //         $("#articles").append("<div data-id='" + data[i]._id + "'>" + 
-// //         "<a href='" + data[i].picture + "'> " + "<h2>" + data[i].title + "</h2>" + "<br />" + 
-// //         "<h5>" + data[i].link + "</h5> </div>");
-// //     }
-// // });
+//Scrape Articles for client to check
+$(document).on("click", "#scrape-button", function() {
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  })
+  window.location.replace("/scrape");
+});
 
-// Whenever someone clicks a p tag
-$(document).on("click", "#savenote", function() {
-    // Empty the notes from the note section
-    console.log("you clicked notes!");
-  
-    $(".note-text").empty();
-    // Save the id from the p tag
+//Delete an article
+$(document).on("click", ".delete-article", function() {
     var thisId = $(this).attr("data-id");
-   
-    // Now make an ajax call for the Article
     $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
+      method: "DELETE",
+      url: "/saved/" + thisId
     })
-      // With that done, add the note information to the page
-      .then(function(data) {
-       // console.log(data);
-        // The title of the article
-        // $("#notes").append("<h2>" + data.title + "</h2>");
-        // // An input to enter a new title
-        // $("#notes").append("<input id='titleinput' name='title' >");
-        // // A textarea to add a new note body
-        // $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // // A button to submit a new note, with the id of the article saved to it
-        // $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
-        }
-      });
-  });
-  
-  // When you click the savenote button
-  $(document).on("click", ".notes", function() {
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      location.reload();
+    });
+});
+
+//Save an article
+$(document).on("click", ".save-article", function() {
+  var thisId = $(this).attr("data-id");
+  $(this).hide();
+  var data = {}
+  data.title =  $("#title-" + thisId).text();
+  data.link = $("#link-" + thisId).text();
+  data.summary = $("#summary-" + thisId).text();
+  $.ajax({
+    method: "POST",
+    dataType: "json",
+    url: "/api/saved",
+    data: data
+  })
+});
+
+//Go to the notes page for a particular article
+$(document).on("click", ".note-comment", function() {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+  window.location.replace("/articles/" + thisId);
+});
+
+  // Submit a note
+  $(document).on("click", "#submit-note", function() {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
+      // Run a POST request to save the note
     $.ajax({
       method: "POST",
       url: "/articles/" + thisId,
       data: {
         // Value taken from title input
-        title: $("#titleinput").val(),
+        title: $("#title-note").val(),
         // Value taken from note textarea
-        body: $("#bodyinput").val()
+        body: $("#note-description").val()
       }
     })
-      // With that done
       .then(function(data) {
         // Log the response
         console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
-  
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
+        window.location.replace("/articles/" + data._id);
+      });  
+      // Also, remove the values entered in the input and textarea for note entry
+      $("#title-note").val("");
+      $("#note-description").val("");
   });
   
+  //delete a note
+  $(document).on("click", ".delete-note", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+      method: "DELETE",
+      url: "/articles/" + thisId
+    })
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      location.reload();
+    }); 
+});
